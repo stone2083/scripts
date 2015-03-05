e3066e8c5932bd26abd216d8834d4577529527a8
Jin Li <li.jinl@taobao.com>
2015-03-05 16:43:07 +0800

    add jettyctl.sh

--- deploy/bin/jettyctl.sh
+++ deploy/bin/jettyctl.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# ===========================
+# jetty ctl start/stop
+# ===========================
+
 function error_handle()
 {
   echo "error @ line ${BASH_LINENO}. exit with status $?"
@@ -10,33 +14,29 @@ set -e
 
 BASE=$(dirname $0)/..
 
-. BASE/bin/env
+. $BASE/bin/env
 
 function config()
 {
-  id=$1
+  ID=$1
   # jetty configuration
-  JETTY_TMPDIR="$JETTY_SERVER_HOME/$id/tmp"
-  JETTY_CONF="$JETTY_SERVER_HOME/$id/conf/jetty.conf"
-  JETTY_WEBAPPS="$JETTY_SERVER_HOME/$id/webapps"
-  START_INI="$JETTY_SERVER_HOME/$id/conf/start.ini"
+  JETTY_TMPDIR="$JETTY_SERVER_HOME/$ID/tmp"
+  JETTY_CONF="$JETTY_SERVER_HOME/$ID/conf"
+  JETTY_WEBAPPS="$JETTY_SERVER_HOME/$ID/webapps"
+  START_INI="$JETTY_SERVER_HOME/$ID/conf/start.ini"
   JETTY_ARGS="--ini=$START_INI"
-  JETTY_LOGS="$JETTY_SERVER_HOME/$id/logs"
-  JETTY_PID="$OUTPUT_HOME/$id/logs/jetty/jetty.pid"
-  ## jvm configuration
-  JAVA="$JAVA_HOME/bin/java"
-  JAVA_OPTIONS="$JAVA_OPTS"
-  export TMPDIR JETTY_CONF JETTY_WEBAPPS JETTY_ARGS START_INI JETTY_PID JETTY_LOGS JAVA_OPTIONS JAVA
+  JETTY_LOGS="$JETTY_SERVER_HOME/$ID/logs"
+  JETTY_PID="$OUTPUT_HOME/$ID/logs/jetty/jetty.pid"
+  # jvm configuration
+  export JAVA="$JAVA_HOME/bin/java"
+  export JAVA_OPTIONS="$JAVA_OPTS"
 }
 
 function prepare()
 {
-  id=$1
-  config $id
-
   # clean jetty server
-  if [ -d "$JETTY_SERVER_HOME" ] ; then
-    rm -rf  "$JETTY_SERVER_HOME"
+  if [ -d "$JETTY_SERVER_HOME/$ID" ] ; then
+    rm -rf  "$JETTY_SERVER_HOME/$ID"
   fi
   mkdir -p "$JETTY_SERVER_HOME"
   mkdir -p "$JETTY_WEBAPPS"
@@ -45,7 +45,7 @@ function prepare()
   mkdir -p "$OUTPUT_HOME/logs/jetty"
 
   # cp file to jetty server home.
-  cp -r "$BASE/conf/jetty/conf" $JETTY_SERVER_HOME/
+  cp -r "$BASE/conf/jetty/conf" $JETTY_CONF
   # TODO: update vars by jetty instance id
 
   rm -rf  "$JETTY_WEBAPPS/root.war"
@@ -54,14 +54,47 @@ function prepare()
 
 function start()
 {
-  id=$1
-  prepare $id
-  $JETTY_HOME/bin/jetty.sh start &> $OUTPUT_HOME/logs/jetty/jetty_stdout.log
+  config $1
+  prepare $1
+  #$JETTY_HOME/bin/jetty.sh start &> $OUTPUT_HOME/logs/jetty/jetty_stdout.log
+  echo "start $ID"
 }
 
 function stop()
 {
-  id=$1
-  prepare $id
-  $JETTY_HOME/bin/jetty.sh stop
+  config $1
+  #$JETTY_HOME/bin/jetty.sh stop
+  echo "stop $ID"
 }
+
+function start_all()
+{
+  for((i=0;i<$JETTY_INSTANCE_NUM;i++))
+  {
+    let id=$i+1
+    start $id
+  }
+}
+
+function stop_all()
+{
+  for((i=0;i<$JETTY_INSTANCE_NUM;i++))
+  {
+    let id=$i+1
+    stop $id
+  }
+}
+
+case $1 in
+  start)
+    start_all
+    ;;
+  stop)
+    stop_all
+    ;;
+  *)
+    echo "Usage: jettyctl.sh start"
+    echo "       jettyctl.sh stop"
+    echo "       jettyctl.sh -h"
+    ;;
+esac
